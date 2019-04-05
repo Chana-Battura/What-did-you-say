9159789f9f4bbdf6e3ecb4fb8e7cdaaf01955efe
Chana-Battura <charanb2005@gmail.com>
2019-04-04 20:20:04 -0700

    Initial commit

---
+++ Bot.py
@@ -0,0 +1,17 @@
+import discord
+
+from profanityfilter import ProfanityFilter
+
+pf = ProfanityFilter()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+
+    async def on_message(self, message):
+        if pf.is_profane(message.content) :
+            await message.channel.send("@"+str(message.author)+" https://giphy.com/gifs/camera-kickstarter-possibilities-91fEJqgdsnu4E")
+
+client = MyClient()
+client.run('NTYzNTIyOTg5NDYzOTYxNjAx.XKajuQ.j7ItSXPgHjxFPOYGoiOF7XRllKQ')
